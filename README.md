207c0f11a3be7c05b98c54d2dc2821da3eb376b2
Ashley <ashleydylanrudd@gmail.com>
2023-02-03 13:37:27 +0000

    v0.0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+# binman
+binman is a Binary File Manager library for C++. Simply include the .cpp and .hpp  
+files in your project.  
+
+binman has been designed to be as lightweight, while being as powerful as possible,  
+It can read, write and modify binary files of any kind.
+
+## Description
+
+## How To Use
+
+## TO-DO 
+
+## Version
+
+## Licence
+BinMan is under the GPL (GPL3.0), please see LICENCE for information  
+(c) ADBeta 2023
---
+++ binman.cpp
@@ -0,0 +1,13 @@
+/*******************************************************************************
+* This file is pat of binman - Binary Managment library for C++
+*
+* If this file is in use in another, unrelated project, please see the github
+* for binman for more information: https://github.com/ADBeta/binman
+*
+* This project is under GNU Public Licence.
+* (c) 2023 ADBeta
+*
+* v0.0.1
+*******************************************************************************/
+
+#include "binman.hpp"
